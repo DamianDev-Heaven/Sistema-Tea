adc1b90cd49a99db9f96805971e8b6e65d8bc4f0
Rebeca Margarita Cruz Polanco <rebeca.cruz@catolica.edu.sv>
2025-05-22 14:19:57 -0600

    Subiendo lista de rol, create y delete

---
+++ Sistema Tea/Controllers/RolController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema_Tea.Models;
+using Sistema_Tea.Models.Data;
+
+namespace Sistema_Tea.Controllers
+{
+    public class RolController : Controller
+    {
+        private readonly TeaContext _context;
+
+        public RolController(TeaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RolController
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _context.Rol.ToListAsync();
+            return View(roles);
+        }
+
+        // GET: RolController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var rol = await _context.Rol.FirstOrDefaultAsync(r => r.RolID == id);
+            if (rol == null)
+                return NotFound();
+
+            return View(rol);
+        }
+
+        // GET: RolController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RolController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Rol rol)
+        {
+             // Esto es solo para probar si guarda sin validación
+             _context.Rol.Add(rol);
+             _context.SaveChanges();
+             return RedirectToAction(nameof(Index));
+
+
+        }
+
+
+        // GET: RolController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var rol = await _context.Rol.FindAsync(id);
+            if (rol == null)
+                return NotFound();
+
+            return View(rol);
+        }
+
+        // POST: RolController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Rol rol)
+        {
+            if (id != rol.RolID)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rol);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "No se pudo guardar los cambios.");
+                }
+            }
+            return View(rol);
+        }
+
+        // GET: RolController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var rol = await _context.Rol.FirstOrDefaultAsync(r => r.RolID == id);
+            if (rol == null)
+                return NotFound();
+
+            return View(rol);
+        }
+
+        // POST: RolController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rol = await _context.Rol.FindAsync(id);
+            if (rol != null)
+            {
+                _context.Rol.Remove(rol);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
+
